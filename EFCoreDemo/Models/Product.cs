83f5c388ee4d243c28afc759632cf6be30cf1143
Nabin Kandel <36199063+Nabinkandel1@users.noreply.github.com>
2025-07-31 11:04:14 +0545

    Add project files.

---
+++ EFCoreDemo/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreDemo.Models
+{
+    public class Product
+    {
+        //PROP
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+    }
+}
