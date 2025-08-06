fc86524f5b34e0afa2366b8c2cab3d7053367baf
Nabin Kandel <36199063+Nabinkandel1@users.noreply.github.com>
2025-08-06 08:34:25 +0545

    Update Migration Script using EF

--- EFCoreDemo/Program.cs
+++ EFCoreDemo/Program.cs
@@ -1,9 +1,14 @@
+using EFCoreDemo;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
